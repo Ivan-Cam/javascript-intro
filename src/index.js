226675f65ca54e3b4a48bdb5176027a3c2353b38
Ivan-Cam <ivan820plus@gmail.com>
2022-02-12 21:30:48 -0600

    array file in progres

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hola')
+const getUser = () => ({
+  id: "ABD123",
+  username: "Tony",
+});
+
+const heroes = [{
+    id: 1,
+    name: 'Batman'
+},{
+    id: 2,
+    name: 'Superman'
+}];
+heroes.filter
+
+const exist = (heroes) => heroes.filter((heroe)=>{heroe.id === 1})
+    
+console.log(exist(heroes)) 
