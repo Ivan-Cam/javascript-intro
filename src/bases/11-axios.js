383c3740c77a3d2382b08da4d8d70c29576fd24f
Ivan-Cam <ivan820plus@gmail.com>
2022-02-17 23:18:46 -0600

    added axios

---
+++ src/bases/11-axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const apiKey = "f0zGmmjCLLMOEnrPb008o6D3s4V3M2K0";
+
+const giphyApi = axios.create({
+  baseURL: "https://api.giphy.com/v1/gifs",
+  params: {
+    api_key: apiKey,
+  },
+});
+
+// giphyApi.get("/random").then((resp) => {
+//   const { data } = resp.data;
+//   const { url } = data.images.original;
+
+//   const img = document.createElement("img");
+//   img.src = url;
+
+//   document.body.append(img);
+// });
